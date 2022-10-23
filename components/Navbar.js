95a8081b6fb595c535ca79a24a2dea2c3ae14c31
pguilhermef <pguilhermesantos@hotmail.com>
2022-10-23 11:54:44 -0300

    feat: Add image render in Navbar

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,4 +1,6 @@
+import Image from "next/image";
 import Link from "next/link";
+import logo_light from "../public/logo_images/logo_light.png"
 
 const navigation = [
   { name: 'Quem sou eu', href: '/' },
@@ -9,6 +11,16 @@ const navigation = [
 function Navbar() {
   return (
     <div>
+      <div>
+      <Link href='/'>
+        <a>
+        <Image
+          src={logo_light}
+          alt="logo_light"
+        />
+        </a>
+      </Link>
+      </div>
       <div>
       {navigation.map((item) => (
         <Link key={item.name} href={item.href}>
