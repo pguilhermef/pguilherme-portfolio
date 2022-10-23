ea4973ae8f5957986d4f8a8c5fa006a2ff76c953
pguilhermef <pguilhermesantos@hotmail.com>
2022-10-23 12:22:15 -0300

    feat: Create Footer component structure

---
+++ components/Footer.js
@@ -0,0 +1,15 @@
+import Link from "next/link";
+
+const links = [
+  { href: 'Quem sou eu', alt: '/' },
+  { href: 'Projetos', alt: '/projects' },
+  { href: 'Wallpapers', alt: '/wallpapers' },
+]
+
+function Footer() {
+  return (
+    <div></div>
+  )
+}
+
+export default Footer
