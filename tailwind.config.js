45d6f69322464c05b5e513c5dac4456cc946cc6b
pguilhermef <pguilhermesantos@hotmail.com>
2023-02-26 22:06:29 -0300

    fix: Fix bugs on tailwind imports and configurations

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,9 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-  ],
+  mode: 'jit',
+  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    colors: {
-      'logo': '#FAFAFA',
-    },
+    extend: {},
   },
+  variants: {},
   plugins: [],
 }
