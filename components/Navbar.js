5086b0a82243f6dc0f6206cb427f6b34cbf0067d
pguilhermef <pguilhermesantos@hotmail.com>
2023-03-17 09:53:44 -0300

    fix: Ajuste nas cores do tailwind e na barra de navegação

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import Link from "next/link";
+import { useState } from "react";
 import logo_light from "../public/logo_images/logo_light.png"
 
 const navigation = [
@@ -8,16 +9,18 @@ const navigation = [
   { name: 'Projetos', href: '#projects' },
   { name: 'Habilidades', href: '#skills' },
   { name: 'Contato', href: '#contact' },
-]
+];
 
 // To do: Criar menu responsivo para telas menores.
 
 function Navbar() {
+  const [isOpen, setIsOpen] = useState(false);
+  
   return (
     <nav className="">
-      <div className="mx-3 p-4">
-        <div className="flex justify-between xl:justify-evenly">
-          
+      <div className="mx-3 p-4 flex justify-center">
+        <div className="flex justify-between xl:justify-evenly w-2/4 bg-red-500">
+          {/* Logo */}
           <div className="flex justify-start items-center">
             <div className="flex jus w-40 xl:w-60">
                 <Link href='/' className="light"> 
@@ -31,7 +34,7 @@ function Navbar() {
                 </Link>
               </div>
           </div>
-
+          {/* Navigation */}
           <div className="flex justify-end items-center">
             <div className="hidden md:flex justify-end items-center space-x-1 xl:space-x-2">
               {navigation.map((item) => (
