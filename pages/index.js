218c0bc73f1b15bc558d08331f53263a3dcc7418
pguilhermef <pguilhermesantos@hotmail.com>
2023-03-19 20:16:33 -0300

    feat: Remove o Head e importa as seções no index.js

--- pages/index.js
+++ pages/index.js
@@ -1,26 +1,19 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
-
 import AboutMe from '../components/AboutMe'
-import Home from '../components/Home'
+import Homepage from '../components/Home'
+import Projects from '../components/Projects'
+import Skills from '../components/Skills'
+import Contact from '../components/Contact'
 
-export default function Index() {
+export default function Home() {
   return (
     <div className='bg-logo'>
-      <div className={styles.container}>
-        <Head>
-          <title>Paulo Guilherme</title>
-          <link rel="icon" href="/favicon.ico" />
-          {/* Box Icons
-          <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'></link>
-          Unicons
-          <link rel="stylesheet" href="https://unicons.iconscout.com/release/v4.0.0/css/line.css"></link> */}
-        </Head>
         <main>
+          <Homepage />
           <AboutMe />
-          <Home />
+          <Projects/>
+          <Skills/>
+          <Contact/>
         </main>
-      </div>
     </div>
   )
 }
