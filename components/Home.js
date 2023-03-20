bcb0dcf79a28ad549f41b989543a2f0081454414
pguilhermef <pguilhermesantos@hotmail.com>
2023-03-19 22:48:59 -0300

    styles: Pequeno ajuste na posição dos elementos para celular

--- components/Home.js
+++ components/Home.js
@@ -22,7 +22,7 @@ function Homepage() {
   </div>
 
   {/* Infos */}
-  <div className='flex flex-col mt-8 lg:mt-0 lg:text-lg'>
+  <div className='flex flex-col mt-5 lg:mt-0 lg:text-lg'>
     <div className='flex'>
       <p className='text-4xl font-semibold'>Paulo Guilherme</p>
     </div>
@@ -40,7 +40,7 @@ function Homepage() {
 
         
         {/* Mouse */}
-        <div className={`flex justify-center my-20 lg:my-24 ${inView ? 'opacity-100' : 'opacity-0'} transition-opacity duration-500`}>
+        <div className={`flex justify-center mb-12 mt-8 lg:my-24 ${inView ? 'opacity-100' : 'opacity-0'} transition-opacity duration-500`}>
           <div className='w-14'>
             <svg className='animate-bounce' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
               <path fill="#000000" d="M12,6a1,1,0,0,0-1,1V9a1,1,0,0,0,2,0V7A1,1,0,0,0,12,6Zm0-4A7,7,0,0,0,5,9v6a7,7,0,0,0,14,0V9A7,7,0,0,0,12,2Zm5,13A5,5,0,0,1,7,15V9A5,5,0,0,1,17,9Z"/>
