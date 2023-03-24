c6e32223b239a5daf7685a1430c812e439548e2f
pguilhermef <pguilhermesantos@hotmail.com>
2023-03-23 23:32:03 -0300

    refactor: Muda foto pessoal da aba about

--- components/AboutMe.js
+++ components/AboutMe.js
@@ -1,7 +1,7 @@
 import Image from 'next/image';
 import React, { useEffect, useState } from 'react';
 
-import myPhoto from '../public/pguilherme.jpeg';
+import myPhoto from '../public/pguilherme_2.jpeg';
 import clock from '../public/about_me_infos/clock.svg';
 import desktop from '../public/about_me_infos/desktop.svg';
 
@@ -39,7 +39,7 @@ function AboutMe() {
         <div className='flex flex-col items-center lg:flex-row lg:justify-evenly lg:items-center lg:h-auto lg:w-5/6 lg:mt-14 lg:mb-10 lg:max-w-screen-xl'>
           {/* Photo */}
           <div className='w-48 lg:w-72 mt-5 flex justify-center'>
-            <div className='flex justify-center rounded-3xl black-and-white'>
+            <div className='flex justify-center rounded-3xl black-and-white shadow-xl'>
               <Image src={myInfo.photo} alt={`Foto de ${myInfo.name}, ${myInfo.profession}`} className='rounded-2xl'/>
             </div>
           </div>
