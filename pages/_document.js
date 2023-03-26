b0e1a92355aab838774b4ee38cb275f19d7f0d0b
pguilhermef <pguilhermesantos@hotmail.com>
2023-03-26 18:42:21 -0300

    style: Fix bug on 'dark-mode' devices

--- pages/_document.js
+++ pages/_document.js
@@ -6,7 +6,7 @@ export default function Document() {
       <Head>
         <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600&display=swap" rel="stylesheet"/>
       </Head>
-      <body>
+      <body className='bg-portfolio-white'>
         <Main />
         <NextScript />
       </body>
